7e6362a5453dc897e4f4f29ca54592422657e28e
romanPashnitskyi <bernald_07@meta.ua>
2019-05-25 20:14:37 +0300

    refactor class component to functional component

--- src/components/title/Title.js
+++ src/components/title/Title.js
@@ -1,14 +1,10 @@
-import React, {Component} from 'react';
+import React from 'react';
 import './Title.scss';
 
-class Title extends Component {
-    render() {
-        return (
-            <div className='title'>
-                <h1>Weather At <span>{this.props.city}</span></h1>
-            </div>
-        );
-    }
-}
+const Title = props => (
+  <div className='title'>
+    <h1>Weather At <span>{props.city}</span></h1>
+  </div>
+);
 
 export default Title;
